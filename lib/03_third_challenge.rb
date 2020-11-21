11833dc1440caa4592e40e7bcdc35190930c002b
John Wisneski <john.j.wisneski@gmail.com>
2020-11-21 22:30:33 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,8 +6,8 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-       :name => "Lord Montague"
-        :age => "53"ur key/value pairs here
+       :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
